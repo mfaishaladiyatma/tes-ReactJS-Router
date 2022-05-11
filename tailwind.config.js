c6e825f8bba4da40520c8bbc0bc5580cb6a151d0
mfaishaladiyatma <mfaishall.adiyatma@gmail.com>
2022-05-11 16:32:44 +0700

    1st page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      rubik: ['Rubik', 'sans-serif']
+    },
     extend: {
       spacing: {
         '13': '3.25rem',
@@ -12,24 +15,29 @@ module.exports = {
       colors: {
         'nyoba-blue': '#1fb6ff',
         'whitesmoke': '#F1F3FF',
-        'background-blue-car': '#0D28A6'
+        'background-blue-car': '#0D28A6',
+        'border-gray': '#D0D0D0'
       },
       width: {
         'screen-1/2': '50vw',
         'screen-1/4': '25vw',
         'screen-45vw': '45vw',
-        '1000px': '1000px'
+        '1000px': '1000px',
+        '75rem': '75rem',
+        '180px': '180px'
       },
       height: {
         'screen-1/2': '50vh',
         'screen-3/4': '75vh',
-        '90%': '90%'
+        '90%': '90%',
+        '100px': '100px'
       },
       maxWidth: {
         '268px': '268px'
       },
       borderRadius: {
-        '60px': '60px'
+        '60px': '60px',
+        '10px': '10px'
       },
       fontSize: {
         'heading-1': ['36px', {
@@ -40,10 +48,14 @@ module.exports = {
         }],
       },
       fontFamily: {
-        'helvetica': ['Helvetica']
+        'helvetica': ['Helvetica', 'sans-serif']
       },
       boxShadow: {
-        'custom-shadow-1': '0px 2px 5px -1px rgba(50, 50, 93, 0.25), 0px 1px 3px -1px rgba(0, 0, 0, 0.3)'
+        'custom-shadow-1': '0px 2px 5px -1px rgba(50, 50, 93, 0.25), 0px 1px 3px -1px rgba(0, 0, 0, 0.3)',
+        'custom-shadow-searchBar': '0px 0px 10px rgba(0, 0, 0, 0.15)'
+      },
+      inset: {
+        '7rem': '7rem'
       }
     },
   },
